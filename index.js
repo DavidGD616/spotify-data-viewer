76480522ad15f8824b3decedf25d1adac17a89f7
David <davidguerrerodiaz12@gmail.com>
2021-10-26 20:48:11 -0700

    Working in authorization

--- index.js
+++ index.js
@@ -56,8 +56,67 @@ app.get('/', (req, res) => {
   });
 
   app.get('/callback', (req, res) => {
-      res.send('callback')
+    const code = req.query.code || null;
+  
+    axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      data: querystring.stringify({
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: REDIRECT_URI
+      }),
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+      },
+    })
+    .then(response => {
+      if (response.status === 200) {
+  
+        const { access_token, token_type } = response.data;
+  
+        const { refresh_token } = response.data;
+
+        axios.get(`http://localhost:8888/refresh_token?refresh_token=${refresh_token}`)
+          .then(response => {
+            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+          })
+          .catch(error => {
+            res.send(error);
+          });
+  
+      } else {
+        res.send(response);
+      }
+    })
+    .catch(error => {
+      res.send(error);
+    });
+  });
+
+ app.get('/refresh_token', (req, res) => {
+  const { refresh_token } = req.query;
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+    },
   })
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(error => {
+      res.send(error);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Express app listening at http://localhost:${port}`)
